f5ef11539c0049f09b4df43dfe21985c5247bdf6
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-12-06 22:51:50 -0800

    updating components with API call results

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCFcII95F05NaEKDguu76AxgCYv5cauqkg';
 
-// Create a new component. This component should produce some html 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />    
-        </div>
-    );
+// class based components changes state
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); // this.setState({ videos: videos });
+        });
+    }
+
+    // data passed from parent (App) to child (VideoList), its called props    
+    render() {
+        return ( // videos in VideoLlist is props
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />    
+            </div>
+        );
+    }
 }
 
 

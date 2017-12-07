b707bce5834cc76cc2dd2aa724142ba2f04dbf8e
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-12-06 16:11:40 -0800

    initial files and setup

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from 'search_bar';
+
+const API_KEY = 'AIzaSyCFcII95F05NaEKDguu76AxgCYv5cauqkg';
+
+// Create a new component. This component should produce some html 
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
